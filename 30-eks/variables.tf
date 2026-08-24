9202452ddc3d621ad2baafd505aeb6d9009844c0
venkey27 <venkey271200@gmail.com>
2026-08-23 22:15:34 -0400

    k8-terraform

--- 30-eks/variables.tf
+++ 30-eks/variables.tf
@@ -7,19 +7,19 @@ variable "environment" {
 }
 
 variable "eks_version" {
-    default = "1.36"
+    default = "1.35"
 }
 
 variable "enable_blue" {
-    default = true
+    default = false
 }
 
 variable "blue_version" {
-    default = "1.36"
+    default = "1.34"
 }
 
 variable "enable_green" {
-    default = false
+    default = true
 }
 
 variable "green_version" {
