6c26b5ca414444e7cb586190022d13d764847247
venkey27 <venkey271200@gmail.com>
2026-08-24 19:43:04 -0400

    K8-eks

--- 30-eks/variables.tf
+++ 30-eks/variables.tf
@@ -11,7 +11,7 @@ variable "eks_version" {
 }
 
 variable "enable_blue" {
-    default = false # if create equals to false then blue will not create. if create equals to true then blue will create
+    default = true # if create equals to false then blue will not create. if create equals to true then blue will create
 }
 
 variable "blue_version" {
@@ -19,7 +19,7 @@ variable "blue_version" {
 }
 
 variable "enable_green" {
-    default = true # if create equals to false then green will not create. if create equals to true then green will create
+    default = false # if create equals to false then green will not create. if create equals to true then green will create
 }
 
 variable "green_version" {
